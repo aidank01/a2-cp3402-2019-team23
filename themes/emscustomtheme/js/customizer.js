9c78dce3caf5354179d23e795ab1b1755c814db0
emersonbaria <emerson.baria@my.jcu.edu.au>
2019-05-05 13:25:36 +1000

    updated theme screenshot for Theme's meta data.

--- themes/emscustomtheme/js/customizer.js
+++ themes/emscustomtheme/js/customizer.js
@@ -14,6 +14,7 @@
 			$( '.site-title a' ).text( to );
 		} );
 	} );
+
 	wp.customize( 'blogdescription', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-description' ).text( to );
